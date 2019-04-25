6910b9e6c867c3d551584181b66170e3782db0af
Gilgamesh <32211128+Dontmindmes@users.noreply.github.com>
2019-04-24 18:32:07 -0700

    Update autostart_windows.go

--- autostart_windows.go
+++ autostart_windows.go
@@ -10,43 +10,23 @@ uint64_t CreateShortcut(char *shortcutA, char *path, char *args);
 */
 import "C"
 
-import (
-	"errors"
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-)
-
 var startupDir string
 
 func init() {
-	startupDir = filepath.Join(os.Getenv("USERPROFILE"), "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+	
 }
 
-func (a *App) path() string {
-	return filepath.Join(startupDir, a.Name+".lnk")
+func (a *App) path()  {
+
 }
 
-func (a *App) IsEnabled() bool {
-	_, err := os.Stat(a.path())
-	return err == nil
+func (a *App) IsEnabled()  {
+
 }
 
-func (a *App) Enable() error {
-	path := a.Exec[0]
-	args := strings.Join(a.Exec[1:], " ")
+func (a *App) Enable()  {
 
-	if err := os.MkdirAll(startupDir, 0777); err != nil {
-		return err
-	}
-	res := C.CreateShortcut(C.CString(a.path()), C.CString(path), C.CString(args))
-	if res != 0 {
-		return errors.New(fmt.Sprintf("autostart: cannot create shortcut '%s' error code: 0x%.8x", a.path(), res))
-	}
-	return nil
 }
 
-func (a *App) Disable() error {
-	return os.Remove(a.path())
+func (a *App) Disable()  {
 }
